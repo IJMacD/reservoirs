e2b06688d5fdaca86a4586c54dac2f58e633d580
Iain MacDonald <IJMacD@gmail.com>
2016-09-13 21:14:52 +0800

    Improve UI and db handling
    Add hot modules;
    app: nicer ui;
    server: store/retrieve database

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+import ReservoirTile from './ReservoirTile';
+import styles from '../styles/App.css';
+
 const RESERVOIR_URL = "/reservoirs.json";
 
 export default class App extends React.Component {
@@ -7,28 +10,41 @@ export default class App extends React.Component {
     super();
     
     this.state = {
+      isLoading: true,
       reservoirs: []
     };
 
     fetch(RESERVOIR_URL)
       .then(result => result.json())
       .then(data => {
-        this.setState({reservoirs: data.reservoirs})
+        this.setState({isLoading: false, reservoirs: data.reservoirs || []})
       });
   }
 
   render () {
-    const { reservoirs } = this.state;
+    const { isLoading, reservoirs } = this.state;
+
+    reservoirs.sort((a,b) => {
+      const a_val = a.utilisation * a.capacity,
+            b_val = b.utilisation * b.capacity;
+      return a_val < b_val ? 1 : (a_val > b_val ? -1 : 0);
+    });
 
     return (
       <div>
-        <ul>
-        {
-          reservoirs.map(reservoir => (
-            <li key={reservoir.name}>{reservoir.name} {reservoir.utilisation * 100}%</li>
-          ))
+        { isLoading ? 
+          <p className={styles.error}>Loading</p> :
+          <ul className={styles.list}>
+          {
+            reservoirs.map(reservoir => (
+              <ReservoirTile key={reservoir.name} reservoir={reservoir} />
+            ))
+          }
+          { reservoirs.length == 0 &&
+            <li className={styles.error}>That's strange but there seem to be no reservoirs</li>
+          }
+          </ul>
         }
-        </ul>
       </div>
     )
   }
---
+++ src/components/ReservoirTile.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+import styles from '../styles/Tile.css';
+
+export default (props) => {
+    const { reservoir } = props;
+    const logCapacity = Math.log(reservoir.capacity);
+    const colorShade = colorMix([200,32,0],[32,128,32],reservoir.utilisation);
+    const tileStyle = {
+        height: logCapacity * 10,
+        background: colorShade,
+    };
+    const percent = `${reservoir.utilisation * 100}%`;
+    const shadeStyle = {
+        background: reservoir.image ? `url(${reservoir.image})` : "rgba(0,0,0,0.4)",
+        backgroundSize: "cover",
+        height: percent,
+        lineHeight: `${reservoir.utilisation * tileStyle.height}px`
+    };
+    const percentStyle = {
+        color: colorShade
+    };
+
+    return (
+        <li className={styles.tile} style={tileStyle}>
+            <span className={styles.shade} style={shadeStyle}></span>
+            <span className={styles.percent} style={percentStyle}>{percent}</span>
+            <span className={styles.name}>{reservoir.name}</span>
+        </li>
+    );
+}
+
+function colorMix(start, end, x){
+  return {
+    0: start[0] + (end[0] - start[0]) * x,
+    1: start[1] + (end[1] - start[1]) * x,
+    2: start[2] + (end[2] - start[2]) * x,
+    toString: function() {
+        return `rgb(
+            ${this[0].toFixed()},
+            ${this[1].toFixed()},
+            ${this[2].toFixed()})`
+    }
+  }
+}
+
+function colorMixA(start, end, x){
+  return {
+    0: start[0] + (end[0] - start[0]) * x,
+    1: start[1] + (end[1] - start[1]) * x,
+    2: start[2] + (end[2] - start[2]) * x,
+    3: start[3] + (end[3] - start[3]) * x,
+    toString: function() {
+        return `rgba(
+            ${this[0].toFixed()},
+            ${this[1].toFixed()},
+            ${this[2].toFixed()},
+            ${this[3]})`
+    }
+  }
+}
